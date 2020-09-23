ba86024797db0131aadbfa732b2520961a72baaf
padulapankaja <padulaguruge@gmail.com>
2020-09-23 17:00:24 +0530

    added

---
+++ app/routes/sessions.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const sessionController = require('./../controllers/sessions.controller');
+
+//get student stats
+router.post('/add', sessionController.add_session);
+
+
+//get student stats
+router.get('/get/allinfo', sessionController.all_info);
+
+module.exports = router;
